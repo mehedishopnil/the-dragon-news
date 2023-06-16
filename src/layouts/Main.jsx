003851f892cf3ac589ab19f45d1ceea5dd768a87
mehedishopnil <mehedihasanshopnil.jr@gmail.com>
2023-06-17 03:49:36 +0600

    setup all the inital things and pages

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../pages/Home/Home/shared/Header/header';
+import Footer from '../pages/Home/Home/shared/Footer/footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
