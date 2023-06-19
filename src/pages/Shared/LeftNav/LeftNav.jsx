ea98fd26a005f4a21f54f11ad121b42f6af6ad8a
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-06-19 11:27:14 +0600

    left side of the site

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './LeftNav.css';
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/categories')
+        .then (res => res.json())
+        .then( data => setCategories(data))
+        .catch(error =>{
+            console.log(error);
+        })
+    },[])
     return (
         <div>
-            <h2>Left Nav</h2>
+            <h4>All Category</h4>
+            <div className='categories'>
+            {
+                categories.map(category => <p
+                key={category.id}
+                >
+                    <Link to={`/category/${category.id}`}>{category.name}</Link>
+                </p>)
+            }
+            </div>
         </div>
     );
 };
